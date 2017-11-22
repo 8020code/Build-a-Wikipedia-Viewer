01ed50da0393f9cdb4bbc7e49d04f5ed12d8b2d1
Hone <hone4292@gmail.com>
2017-11-22 13:03:39 +0200

    can search Wikipedia entries in a search box and see the resulting Wikipedia entries and click a button to see a random Wikipedia entry.

--- scripts/index.js
+++ scripts/index.js
@@ -4,27 +4,29 @@ $(document).ready(function () {
 
   $("#f1").submit(function (event) {
     event.preventDefault();
-    search($("#st").val()); 
-    console.log('submit');    
-  });
+    search($("#st").val());
+    console.log('submit');
+  }); 
   var search = function (q) {
-    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=" + q, function (data) {
-      var pagesJSON = data.query.pages;
-      console.log(pagesJSON);
+    $.getJSON("https://en.wikipedia.org/w/api.php?action=query&list=search&utf8=&origin=*&format=json&srsearch=" + q, function (data) {
+      var query, search, title, pageid, snippet;
+      var pagesJSON = data.query.search;
+      console.log(data);
 
       var keys = Object.keys(pagesJSON);
       if (data !== undefined) {
         $("#done").empty();
-        $("#done").append('<p>Results<p/>');
+        $("#done").append('<h1>Results</h1>');
         for (var i in pagesJSON) {
-          console.log(pagesJSON[i].title);
-          var pageid = JSON.stringify(pagesJSON[i].title);
-          
+          console.log(pagesJSON[i].title);          
 
-          $("#done").append('<a target="_blank" href="http://en.wikipedia.org/?curid=' + pagesJSON[i].pageid + '">' + pagesJSON[i].title + '</a><br>');
+          $("#done").append('<h2><a target="_blank" href="http://en.wikipedia.org/?curid=' + pagesJSON[i].pageid + '">' + pagesJSON[i].title + '</a></h2><br><h3>' + pagesJSON[i].snippet +'</h3><br>');
         }
       }
     });
+
   };
+
+
 });
 
